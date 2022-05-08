247bba406f89fa87a232b141d0d78f5ed89ce80f
yuu <meny1122men@gmail.com>
2022-05-08 07:14:53 +0000

    [update]5-19 いいね機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-    
+
   devise_for :users
-  
+
   root to: 'homes#top'
-  
+
   get 'homes/about' => 'homes#about', as: :about
-  
+
   resources :users, only: [:show, :edit, :update]
-  
+
   resources :post_images, only: [:new, :index, :show, :create, :destroy] do
     resources :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
